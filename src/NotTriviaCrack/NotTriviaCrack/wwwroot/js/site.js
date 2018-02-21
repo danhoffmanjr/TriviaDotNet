45cfaef26587dea0ec234e21375d77e8586190ac
Dan Hoffman <dghoffman@gmail.com>
2018-02-21 12:44:02 -0500

    removed table from Admin index page. Added show/hide toggle function to details with jQuery.

--- src/NotTriviaCrack/NotTriviaCrack/wwwroot/js/site.js
+++ src/NotTriviaCrack/NotTriviaCrack/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿// Write your JavaScript code.
+﻿$( function() {
+    function toggleDetails(id) {
+        // get effect type from
+        var selectedEffect = $("#effectTypes").val();
+
+        // Most effect types need no options passed by default
+        var options = {};
+        // some effects have required parameters
+        if (selectedEffect === "scale") {
+            options = { percent: 50 };
+        } else if (selectedEffect === "size") {
+            options = { to: { width: 200, height: 60 } };
+        }
+        // Run the effect
+        $("#details-" + id).toggle(selectedEffect, options, 500);
+    };
+
+    $(".toggle-details").on("click", function () {
+        var txt = $(this).text() == 'Show Details' ? 'Hide Details' : 'Show Details';
+        $(this).text(txt);
+        toggleDetails($(this).data('id'));
+    });
+});
